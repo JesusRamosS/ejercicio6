6e6acedfd8ccbb3dc57a8a819a4a565cc20671fe
JesusRamosS <scapa.a.secas@gmail.com>
2018-10-03 12:30:23 +0200

    ahora ademas muestra mi lugar de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,6 @@
 public class Prueba{
   public static void main(String args[]){
-   System.out.println("Jesus Ramos Seca"); 
+   System.out.println("Jesus Ramos Seca");
+   System.out.println("Rioseco de Tapia");	
  } 
 }
