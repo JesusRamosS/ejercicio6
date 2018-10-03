ce663c13b81469e6849f824d17fed516caf4cd25
JesusRamosS <scapa.a.secas@gmail.com>
2018-10-03 12:31:41 +0200

    ahora ademas muestra mi lugar de residencia

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,7 @@
 public class Prueba{
   public static void main(String args[]){
    System.out.println("Jesus Ramos Seca");
-   System.out.println("Rioseco de Tapia");	
+   System.out.println("Rioseco de Tapia");
+   System.out.println("Residencia: Rioseco de Tapia");	
  } 
 }
